68c740644ef8e26f06748df0911bc637d7153906
Lewis De La Rosa <jtinfinity99@gmail.com>
2020-10-28 13:05:42 -0400

    message

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,8 +6,15 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+  def age_appropriate_birthday(birthday_kids)
+    birthday_kids.each do |name, age|
+      if age <= 12
+        puts "Happy Birthday #{name}! You are now #{age} years old!"
+      else
+        puts "You are too old for this."
+      end
+    end
   end
 end
-
-
